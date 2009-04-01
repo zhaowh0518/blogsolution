2c6438e97b40b2303484d1d98e2967b8f480fedd
disappearwind <disappearwind@bfa58db6-1ea3-11de-ac9f-1d9250355bca>
2009-04-01 10:55:51 +0000

    add interface of blog
    add YgBlogEntity but didn't implement GetPosts

---
+++ trunk/BlogSolution/trunk/IBlog/IPost.cs
@@ -0,0 +1,36 @@
+﻿/****************************************************************************
+ * Copyright (C) Disappearwind. All rights reserved.                        *
+ *                                                                          *
+ * Author: disapearwind, disappearwind@gmail.com                            *
+ * Created: 2009-4-1                                                        *
+ *                                                                          *
+ * Description:                                                             *
+ *   Definition interface for base post.                                    *
+ *                                                                          *
+*****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlogSolution.IBlog
+{
+    /// <summary>
+    /// post interface
+    /// </summary>
+    public interface IPost
+    {
+        /// <summary>
+        /// Post title
+        /// </summary>
+        string Title { get; set; }
+        /// <summary>
+        /// Post content
+        /// </summary>
+        string Content { get; set; }
+        /// <summary>
+        /// Post createdate,maybe didn't today,it was the actual created day.
+        /// </summary>
+        DateTime CreateDate { get; set; }
+    }
+}
---
+++ trunk/BlogSolution/trunk/YgBlogEntity/YgBlog.cs
@@ -0,0 +1,71 @@
+﻿/****************************************************************************
+ * Copyright (C) Disappearwind. All rights reserved.                        *
+ *                                                                          *
+ * Author: disapearwind, disappearwind@gmail.com                            *
+ * Created: 2009-4-1                                                        *
+ *                                                                          *
+ * Description:                                                             *
+ *   Definition ygblog.                                                     *
+ *   ChineseName:阳光博客                                                   *
+ *   BlogURL:http://disappearwind.ygblog.com                                *
+ *                                                                          *
+*****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlogSolution.IBlog;
+
+namespace BlogSolution.YgBlogEntity
+{
+    /// <summary>
+    /// yblog
+    /// </summary>
+    public class YgBlog : IBaseBlog
+    {
+        #region IBaseBlog Members
+        /// <summary>
+        /// ygblog
+        /// </summary>
+        public string BlogName
+        {
+            get { return "ygblog"; }
+        }
+        /// <summary>
+        /// http://disappearwind.ygblog.com
+        /// </summary>
+        public string URL
+        {
+            get { return "http://disappearwind.ygblog.com"; }
+        }
+        /// <summary>
+        /// yg
+        /// </summary>
+        public string KeyWord
+        {
+            get { return "ygblog"; }
+        }
+        /// <summary>
+        /// Add a post to ygblog
+        /// </summary>
+        /// <param name="post"></param>
+        public void AddPost(IPost post)
+        {
+            throw new NotImplementedException("The ygbog will not implement AddPostMethod");
+        }
+        /// <summary>
+        /// Get posts from xml files
+        /// </summary>
+        /// <param name="xmlPost"></param>
+        /// <returns></returns>
+        public List<IPost> GetPosts(string xmlPost)
+        {
+            List<IPost> postList = new List<IPost>();
+            YgPost p1 = new YgPost();
+            postList.Add(p1);
+            return postList;
+        }
+
+        #endregion
+    }
+}
