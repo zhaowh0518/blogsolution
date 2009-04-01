86b1342c88c560315bd480fecdd17197c310d3dc
disappearwind <disappearwind@bfa58db6-1ea3-11de-ac9f-1d9250355bca>
2009-04-01 10:55:51 +0000

    add interface of blog
    add YgBlogEntity but didn't implement GetPosts

---
+++ BlogSolution/trunk/IBlog/IBaseBlog.cs
@@ -0,0 +1,47 @@
+﻿/****************************************************************************
+ * Copyright (C) Disappearwind. All rights reserved.                        *
+ *                                                                          *
+ * Author: disapearwind, disappearwind@gmail.com                            *
+ * Created: 2009-4-1                                                        *
+ *                                                                          *
+ * Description:                                                             *
+ *   Definition interface for base blog.                                    *
+ *                                                                          *
+*****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlogSolution.IBlog
+{
+    /// <summary>
+    /// Base blog interface
+    /// </summary>
+    public interface IBaseBlog
+    {
+        /// <summary>
+        /// Blog name,the type is string and it's readonly
+        /// </summary>
+        string BlogName { get;}
+        /// <summary>
+        /// Blog url,the url which the blog can access and it's readonly
+        /// </summary>
+        string URL { get; }
+        /// <summary>
+        /// Keyword assign to the blog
+        /// </summary>
+        string KeyWord { get; }
+        /// <summary>
+        /// Add a post to the blog
+        /// </summary>
+        /// <param name="post">the post to be added,it must implement IPost</param>
+        void AddPost(IPost post);
+        /// <summary>
+        /// Get post from xml
+        /// </summary>
+        /// <param name="xmlPost">xml</param>
+        /// <returns>posts list</returns>
+        List<IPost> GetPosts(string xmlPost);
+    }
+}
---
+++ BlogSolution/trunk/YgBlogEntity/YgPost.cs
@@ -0,0 +1,61 @@
+﻿/****************************************************************************
+ * Copyright (C) Disappearwind. All rights reserved.                        *
+ *                                                                          *
+ * Author: disapearwind, disappearwind@gmail.com                            *
+ * Created: 2009-4-1                                                        *
+ *                                                                          *
+ * Description:                                                             *
+ *   Definition the post of ygblog.                                         *
+ *                                                                          *
+*****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlogSolution.IBlog;
+
+namespace BlogSolution.YgBlogEntity
+{
+    public class YgPost:IPost
+    {
+        #region IPost Members
+
+        public string Title
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public DateTime CreateDate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+    }
+}
